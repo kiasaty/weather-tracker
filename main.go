644a6e2eaef80f4f23d4a3d024823a18ed01a388
Ehsan Kiasaty <e.kiasaty@gmail.com>
2023-11-24 00:13:48 +0100

    Create struct for Coordinates

--- main.go
+++ main.go
@@ -22,29 +22,29 @@ func main() {
 }
 
 func fetchWeathers(w http.ResponseWriter, req *http.Request) {
-	locations := make(map[string][2]float64)
+	locations := make(map[string]models.Coordinate)
 
-	locations["Berlin"] = [2]float64{52.520008, 13.404954}
-	locations["Paris"] = [2]float64{48.864716, 2.349014}
-	locations["Rome"] = [2]float64{41.902782, 12.496366}
-	locations["Lisbon"] = [2]float64{38.736946, -9.142685}
+	locations["Berlin"] = models.Coordinate{Latitude: 52.520008, Longitude: 13.404954}
+	locations["Paris"] = models.Coordinate{Latitude: 48.864716, Longitude: 2.349014}
+	locations["Rome"] = models.Coordinate{Latitude: 41.902782, Longitude: 12.496366}
+	locations["Lisbon"] = models.Coordinate{Latitude: 38.736946, Longitude: -9.142685}
 
 	var wg sync.WaitGroup
 
-	for locationName, coordinates := range locations {
+	for locationName, coordinate := range locations {
 		wg.Add(1)
-		go fetchWeather(locationName, coordinates, &wg)
+		go fetchWeather(locationName, coordinate, &wg)
 	}
 
 	wg.Wait()
 }
 
-func fetchWeather(locationName string, coordinates [2]float64, wg *sync.WaitGroup) {
+func fetchWeather(locationName string, coordinate models.Coordinate, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	currentWeatherData := openweathermap.GetCurrentWeather(
-		coordinates[0],
-		coordinates[1],
+		coordinate.Latitude,
+		coordinate.Latitude,
 	)
 
 	db := database.Connect()
